6bbdad39a9b69f34d0a4d9711a8927f792f879dc
Brenno Siqueira <140553743+BrennoDS@users.noreply.github.com>
2026-01-26 12:34:11 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,7 +3,6 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,9 +26,8 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
-	
-	@OneToMany(mappedBy = "client")
 	@JsonIgnore
+	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
@@ -90,7 +88,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -102,11 +103,15 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-
-	
-	
-	
 }
+
+
+	
+
